18ad001423f838ad12b05d1be5ffd2428b7216ba
Hamzenis Kryeziu <hamzenis@gmail.com>
2022-12-12 15:33:09 +0100

    von Anfang

--- LibraryManagementSystem/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.lms.librarymanagementsystem {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.lms.librarymanagementsystem to javafx.fxml;
-    exports com.lms.librarymanagementsystem;
-}
