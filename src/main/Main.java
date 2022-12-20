2684cfc26d33afa0da48ab67de0a8c3126c8a3ed
Barry <baba.barry@stud.fra-uas.de>
2022-12-20 16:49:01 +0100

    Added Libraian GUI

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
         //  Only Debug Terminal Output
         System.out.println("Hello World");
+        System.out.println("Test");
 
         //  Program start code
         Initial startProject = new Initial();
