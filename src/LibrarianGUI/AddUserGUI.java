5068f73b5b81bb610024468e2ddd3efbd8194a29
Kejvi21 <kejvi.hysenbelli@stud.fra-uas.de>
2023-01-10 19:21:47 +0100

    Admin Panel design

--- src/LibrarianGUI/AddUserGUI.java
+++ src/LibrarianGUI/AddUserGUI.java
@@ -1,8 +1,14 @@
 package src.LibrarianGUI;
 
+import Project.ConnectionProvider;
 import src.main.Initial;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+import Project.*;
 
 public class AddUserGUI extends JFrame{
     private JTextField inputUserID;
@@ -19,6 +25,31 @@ public class AddUserGUI extends JFrame{
             setVisible(false);
             dispose();
             in.startLibrarianGUI();
+        });
+        saveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //String id_user = inputUserID.getText();
+                String fname_user = inputUsername.getText();
+                String lname_user = inputLastname.getText();
+                String typeof_user = (String) inputTypeUser.getSelectedItem();
+                try
+                {
+                    Connection con = ConnectionProvider.getCon();
+                    Statement st = con.createStatement();
+                    // st.executeUpdate("insert into user values ('"+id_user+"','"+fname_user+"','"+lname_user+"','"+typeof_user+"')");
+                    st.executeUpdate("insert into Library_Management_System.user (First_name,Last_name,Is_Librarian) values ('"+fname_user+"','"+lname_user+"','"+typeof_user+"')");
+                    JOptionPane.showMessageDialog(null,"Successfully Updated");
+                    setVisible (false);
+                    new AddUserGUI().setVisible (true);
+                }
+                catch(Exception evt)
+                {
+                    JOptionPane.showMessageDialog(null,"User Id already exists");
+                    //setVisible(false);
+                    new AddUserGUI().setVisible (true);
+                }
+            }
         });
     }
 
