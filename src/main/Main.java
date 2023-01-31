059872f3e83572885a67a6a50b2a4b18dd48eaf1
Hamzenis Kryeziu <hamzenis@gmail.com>
2023-01-31 20:58:15 +0100

    AddBookGUI finished and works - Added Comments in TODO - Cleaned up code and comments - Linked Login with Database - Redesigned IssuedBooks.java from userGUI
    Signed-off-by: Hamzenis Kryeziu <hamzenis@gmail.com>

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,13 +2,16 @@ package src.main;
 
 
 /*      TODO:
- *   - Add save button functionality in every GUI window
- *   - Connect LoginGUI with User Table Mysql
- *   - Link Genre JCombobox with Database in AddBookGUI
- *   - Change code in createTable() && searchBookAuthor() with PreparedStatement
- *   - Buttons with enter input
- *   - Return a Book!!!!!!!!! in EntriesGUI with a table to view returned Books
- *   - Add a table history in database!!!!! cc of borrow with returnedDate
+ *  - Link Genre JCombobox with Database in AddBookGUI
+ *  - Change code in createTable() && searchBookAuthor() with PreparedStatement
+ *  - Buttons with enter input
+ *  - Return a Book!!!!!!!!! in EntriesGUI with a table to view returned Books
+ *  - Add a table history in database!!!!! cc of borrow with returnedDate
+ *  - IssuedBooks userGUI Table
+ *  - ReturnedBook GUI
+ *  - Add in Database History Books table (as borrowedTable)
+ *  - EntriesGUI and database logic
+ *  - Issue Book GUI anpassen und logic
  *
  *      Backlog (later TODO):
  *          - Add possibility to add more than one author per book
